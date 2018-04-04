45e39cc57611e0d640f4480bc8feb47b29130d5b
刘鹤 <liuhe@csdn.net>
2018-04-04 18:12:10 +0800

    测试框架才玩会

--- test/response.test.js
+++ test/response.test.js
@@ -1,8 +1,14 @@
-let mocha = require('mocha');
 let should = require('should');
 let response = require('../src/response');
-describe('ok',function(){
-  it('should return ok', function() {
-    should(response.body()).startWith('liuhe');
+const res = response;
+describe('response body/',function(){
+  it('test set body,should return liuhe', function() {
+    // should(res.body= 'liuhe').be.a.String()
+    should(res.body= 'liuhe').be.exactly('liuhe').and.be.a.String()
+  });
+  it('test get body,should return liuhe', function() {
+    res.body= 'liuhe'
+    should(res.body).be.exactly('liuhe').and.be.a.String()
+    // should(res.body).be.a.String()
   });
 })
